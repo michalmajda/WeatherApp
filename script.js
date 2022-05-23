13b055e135b4a74560c79069335b62a245a7450b
michal <michal.majda.11@gmail.com>
2022-05-23 22:32:03 +0200

    Api added, style changed, some improves in html file

--- script.js
+++ script.js
@@ -6,3 +6,62 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q="
+const API_KEY = "&appid=f5c9045d1cf448e348694706e627d813"
+const API_UNITS = "&units=metric"
+
+const getWeather = () => {
+    const city = input.value || "London"
+    const URL = API_LINK + city + API_KEY + API_UNITS
+
+    axios.get(URL).then(res => {
+        console.log(res.data)
+        const temp = res.data.main.temp
+        const hum = res.data.main.humidity
+        const status = res.data.weather[0]
+        warning.textContent = ""
+        
+
+
+        if(status.id >= 200 && status.id <300){
+            photo.setAttribute('src', './img/thunderstorm.png')
+        } else if(status.id >= 300 && status.id <400){
+            photo.setAttribute('src', './img/drizzle.png')
+        } else if(status.id >= 500 && status.id <600){
+            photo.setAttribute('src', './img/rain.png')
+        } else if(status.id >= 600 && status.id <700){
+            photo.setAttribute('src', './img/ice.png')
+        } else if(status.id >= 700 && status.id <800){
+            photo.setAttribute('src', './img/fog.png')
+        }else if(status.id == 800){
+            photo.setAttribute('src', './img/sun.png')
+        } else if(status.id >= 800 && status.id < 900){
+            photo.setAttribute('src', './img/cloud.png')
+        } else {
+            photo.setAttribute('src', './img/unknown.png')
+        }
+        
+        cityName.textContent = res.data.name
+        temperature.textContent = Math.floor(temp) + "°C"
+        humidity.textContent = hum + "%"
+        weather.textContent = status.main
+
+    }).catch(() => { 
+        warning.textContent = "Invalid city name!"        
+    })
+
+    input.value = ""
+    
+}
+
+const enterCheck = (e) => {
+    if(e.key === 'Enter'){
+        getWeather()
+    }
+}
+
+
+getWeather()
+input.addEventListener("keyup", enterCheck)
+button.addEventListener('click', getWeather)
